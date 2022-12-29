b55366c09b6b30e36be95c41fd9402770dab4f32
Vikrantpalle <vikrantpalle@gmail.com>
2022-12-30 01:05:35 +0530

    added docs and error handling

--- videoServer/server.go
+++ videoServer/server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
@@ -12,50 +13,48 @@ import (
 
 type Segment struct {
 	Duration float32
-	RelUrl string
+	RelUrl   string
 }
 
-func min(a,b int) int {
-	if a<b {
+func min(a, b int) int {
+	if a < b {
 		return a
 	}
 	return b
 }
 
-func cors (fs http.Handler) http.HandlerFunc {
-	return func (w http.ResponseWriter, r *http.Request)  {
+func cors(fs http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		fs.ServeHTTP(w,r)
+		fs.ServeHTTP(w, r)
 	}
 }
 
-var segment_list = make([]Segment, 0)
+func getSegments(path string) ([]Segment, error) {
 
-func getSegments() {
-	f,err := os.Open("C:/Users/vikra/go-video/video/tenet/file.m3u8")
+	var segment_list = make([]Segment, 0)
+	f, err := os.Open(path)
 	if err != nil {
-		fmt.Println(err)
+		return segment_list, fmt.Errorf("getSegments: could not open file: %w", err)
 	}
 
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
 
-	line :=1
-
-
+	line := 1
 
 	for scanner.Scan() {
 		if strings.HasPrefix(scanner.Text(), "#EXTINF:") {
 			var newSegment Segment
-			dur,err := strconv.ParseFloat(strings.TrimSuffix(strings.TrimPrefix(scanner.Text(), "#EXTINF:"), ","), 32)
-			if err!=nil {
-				fmt.Println(err)
+			dur, err := strconv.ParseFloat(strings.TrimSuffix(strings.TrimPrefix(scanner.Text(), "#EXTINF:"), ","), 32)
+			if err != nil {
+				return segment_list, fmt.Errorf("getSegments: could not parse #EXTINF: %w", err)
 			}
-			
+
 			scanner.Scan()
-			if err := scanner.Err();err!=nil {
-				fmt.Println(err)
+			if err := scanner.Err(); err != nil {
+				return segment_list, fmt.Errorf("getSegments: %w", err)
 			}
 			RelUrl := scanner.Text()
 
@@ -67,38 +66,51 @@ func getSegments() {
 		line++
 	}
 
-	if err = scanner.Err(); err!=nil {
-		fmt.Println(err)
+	if err = scanner.Err(); err != nil {
+		return segment_list, fmt.Errorf("getSegments: %w", err)
 	}
 
+	return segment_list, nil
 }
 
-func updatePlaylist(){
-	
+func updatePlaylist(segment_list []Segment, path string) error {
+
+	out_path := filepath.Dir(path) + "/output.m3u8"
 	media_seq := 0
-	for media_seq < len(segment_list){
-		f,err := os.Create("C:/Users/vikra/go-video/video/tenet/output.m3u8")
-		if err !=nil {
-			fmt.Println(err)
+	for media_seq < len(segment_list) {
+		f, err := os.Create(out_path)
+		if err != nil {
+			return fmt.Errorf("updatePlaylist: could not create file: %w", err)
 		}
 		w := bufio.NewWriter(f)
-	w.WriteString("#EXTM3U\n#EXT-X-VERSION:3\n#EXT-X-TARGETDURATION:13\n#EXT-X-MEDIA-SEQUENCE:"+strconv.Itoa(media_seq)+"\n")	
-	for i:= media_seq; i < min(media_seq+4, len(segment_list));i++ {
-		w.WriteString("#EXTINF:"+strconv.FormatFloat(float64(segment_list[i].Duration),'f',-1,32)+",\n"+segment_list[i].RelUrl+"\n")
-	}
-	w.Flush()
-	f.Close()
-	c1 := time.NewTimer( time.Duration(segment_list[media_seq].Duration * float32(time.Second)))
+		w.WriteString("#EXTM3U\n#EXT-X-VERSION:3\n#EXT-X-TARGETDURATION:13\n#EXT-X-MEDIA-SEQUENCE:" + strconv.Itoa(media_seq) + "\n")
+		for i := media_seq; i < min(media_seq+4, len(segment_list)); i++ {
+			w.WriteString("#EXTINF:" + strconv.FormatFloat(float64(segment_list[i].Duration), 'f', -1, 32) + ",\n" + segment_list[i].RelUrl + "\n")
+		}
+		w.Flush()
+		f.Close()
+		c1 := time.NewTimer(time.Duration(segment_list[media_seq].Duration * float32(time.Second)))
 
-	<-c1.C
-	media_seq++
+		<-c1.C
+		media_seq++
+	}
+
+	return nil
+}
+
+func streamVideo(path string) {
+	segment_list, err := getSegments(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = updatePlaylist(segment_list, path)
+	if err != nil {
+		fmt.Println(err)
 	}
 }
 
-
 func main() {
-	getSegments()
-	go updatePlaylist()
+	go streamVideo("C:/Users/vikra/go-video/video/tenet/file.m3u8")
 
 	videoFs := http.FileServer(http.Dir("/Users/vikra/go-video"))
 
